f95624948e48d3fa2f87b6ec3a3b9852d2d6aaf7
StoyanNikolchev <nikolchev98@protonmail.com>
2024-10-08 18:05:03 +0300

    Clean up code.

--- src/Main.java
+++ src/Main.java
@@ -9,44 +9,51 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+
+    private static final String DESKTOP_PATH = System.getProperty("user.home") + "\\Desktop";
+    private static final String FIRST_SUBFOLDER_PATH = DESKTOP_PATH + "\\Stuff";
+    public static final String CURRENT_DATE = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    public static final String TARGET_FOLDER_PATH = FIRST_SUBFOLDER_PATH + "\\" + CURRENT_DATE;
+
     public static void main(String[] args) {
-        String desktopPath = System.getProperty("user.home") + "\\Desktop";
-        File desktopFolder = new File(desktopPath);
+        File desktopFolder = new File(DESKTOP_PATH);
 
-        List<String> blacklist = Arrays.stream(Blacklist.values())
-                .map(Blacklist::getFileName)
-                .toList();
+        File[] files = getNonBlacklistedFiles(desktopFolder);
 
-        File[] files = desktopFolder.listFiles(file -> file.isFile() && !blacklist.contains(file.getName()));
-
-        if (files == null || files.length == 0) {
+        //Stops the program if there are no eligible files
+        if (files == null) {
             System.out.println("No files to move.");
             return;
         }
 
-        String firstSubfolderPath = desktopPath + "\\Stuff";
-        File firstSubfolder = new File(firstSubfolderPath);
+        File firstSubfolder = new File(FIRST_SUBFOLDER_PATH);
+        createDirectoryIfMissing(firstSubfolder);
 
-        if (!firstSubfolder.exists()) {
-            firstSubfolder.mkdirs();
-        }
-
-        String currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-        String targetFolderPath = firstSubfolderPath + "\\" + currentDate;
-        File targetFolder = new File(targetFolderPath);
-
-        if (!targetFolder.exists()) {
-            targetFolder.mkdirs();
-        }
+        File targetFolder = new File(TARGET_FOLDER_PATH);
+        createDirectoryIfMissing(targetFolder);
 
         for (File file : files) {
             try {
                 Path newPath = new File(targetFolder, file.getName()).toPath();
                 Files.move(file.toPath(), newPath, StandardCopyOption.REPLACE_EXISTING);
-                System.out.println("Moved " + file.getName() + " to " + targetFolderPath);
+                System.out.println("Moved " + file.getName() + " to " + TARGET_FOLDER_PATH);
             } catch (IOException ioException) {
                 System.out.println("Could not move " + file.getName() + ": " + ioException.getMessage());
             }
         }
+    }
+
+    private static void createDirectoryIfMissing(File firstSubfolder) {
+        if (!firstSubfolder.exists()) {
+            firstSubfolder.mkdirs();
+        }
+    }
+
+    private static File[] getNonBlacklistedFiles(File desktopFolder) {
+        List<String> blacklist = Arrays.stream(Blacklist.values())
+                .map(Blacklist::getFileName)
+                .toList();
+
+        return desktopFolder.listFiles(file -> file.isFile() && !blacklist.contains(file.getName()));
     }
 }
